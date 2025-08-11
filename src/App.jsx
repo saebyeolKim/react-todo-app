92da2515e5b37491642452e5d65824be4d321a61
saebyeolKim <toquf2611@naver.com>
2025-08-11 21:06:53 +0900

    Map 메소드를 사용한 할 일 목록 나열

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,38 @@ import { Component } from 'react'
 // 클래스형 컴포넌트 만들기
 // Component : react 내부에서 만든 것을 상속받은 것 -> render 메소드도 바로 사용할 수 있는 이유
 export default class App extends Component {
+
+  // 배열 안에 객체
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true,
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false,
+    },
+  ]
+
+  btnStyle = {
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+  }
+  // 값을 받아서 dynamic 하게 사용 가능
+  getStyle = () => {
+    return {
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoraion: 'none',
+    }
+  }
+
   render() {
     return (
       <div className='container'>
@@ -11,6 +43,15 @@ export default class App extends Component {
           <div className='title'>
             <h1>할 일 목록</h1>
           </div>
+      {
+        this.todoData.map((data) => (
+          <div key={data.id} style={this.getStyle()}>
+            <input type='checkbox' defaultChecked={data.completed}/>
+            {data.title}
+            <button style={this.btnStyle}>X</button>
+          </div>
+        ))
+      }
         </div>
       </div>
     )
